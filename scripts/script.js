f21b647b99446ddeea00c72e6135ecbc6b657b2f
Luiz Felipe <luizfpsena04@gmail.com>
2024-09-29 22:34:00 -0300

    exercicio 4

--- scripts/script.js
+++ scripts/script.js
@@ -1,38 +1,80 @@
-function changeTextColor() {
-    const body = document.body;
-    const select = document.getElementById("select-color");
+let originalRoastingHTML = '';
 
-    const optionColor = document.getElementById("select-color").value;
+window.onload = function () {
+    originalRoastingHTML = document.querySelector(".form-roasting").innerHTML;
+};
 
-    switch (optionColor) {
-        case 'red':
-            body.style.color = "#FF0000";
-            select.style.color = "#FF0000";
-            select.style.borderColor = "#FF0000";
-            break;
-        case 'green':
-            body.style.color = "#129700";
-            select.style.color = "#129700";
-            select.style.borderColor = "#129700";
-            break;
-        case 'blue':
-            body.style.color = "#033694";
-            select.style.color = "#033694";
-            select.style.borderColor = "#033694";
-            break;
-        case 'yellow':
-            body.style.color = "#FFDB10";
-            select.style.color = "#FFDB10";
-            select.style.borderColor = "#FFDB10";
-            break;
-        case 'purple':
-            body.style.color = "#7900EB";
-            select.style.color = "#7900EB";
-            select.style.borderColor = "#7900EB";
-            break;
-        default:
-            body.style.color = "#000000";
-            select.style.color = "#000000";
-            select.style.borderColor = "#000000";
+function selectDrink(drinkId) {
+    const drinkRadio = document.getElementById(drinkId);
+    drinkRadio.checked = true;
+
+    drinkChosen();
+}
+
+function drinkChosen() {
+    const formRoasting = document.querySelector('.form-roasting');
+
+    const drink = document.querySelector('input[name="drink"]:checked').value;
+
+    if (drink.toLowerCase() == "chocolate") {
+        formRoasting.innerHTML = '';
+    } else {
+        formRoasting.innerHTML = originalRoastingHTML;
     }
+}
+
+function submitOrder() {
+    const selectedDrink = document.querySelector('input[name="drink"]:checked');
+    if (!selectedDrink) {
+        const orderSummary = "<b>Selecione sua bebida</b>";
+
+        document.getElementById('alertMessage').innerHTML = orderSummary;
+        document.getElementById('customAlert').style.display = 'flex';
+
+        resetForm();
+        return;
+    }
+
+    let selectedRoasting = document.querySelector('input[name="roasting"]:checked');
+    selectedRoasting = selectedRoasting ? selectedRoasting.value : 'Nenhuma';
+
+    const selectedGarnishes = [];
+    document.querySelectorAll('.garnish-container input[type="checkbox"]').forEach(checkbox => {
+        if (checkbox.checked) {
+            selectedGarnishes.push(checkbox.nextElementSibling.textContent.trim());
+        }
+    });
+
+    const orderSummary = `
+    <b>Pedido realizado com sucesso!</b><br><br>
+    <b>Bebida selecionada:</b> ${selectedDrink.value}<br>
+    <b>Torra selecionada:</b> ${selectedRoasting}<br>
+    <b>Acompanhamentos:</b> ${selectedGarnishes.length > 0 ? selectedGarnishes.join(', ') : 'Nenhum'}
+    `;
+
+    document.getElementById('alertMessage').innerHTML = orderSummary;
+    document.getElementById('customAlert').style.display = 'flex';
+
+    resetForm();
+}
+
+function resetForm() {
+    document.querySelectorAll('input[name="drink"]').forEach(radio => {
+        radio.checked = false;
+    });
+
+    document.querySelectorAll('input[name="roasting"]').forEach(radio => {
+        radio.checked = false;
+    });
+
+    document.querySelectorAll('.garnish-container input[type="checkbox"]').forEach(checkbox => {
+        checkbox.checked = false;
+    });
+}
+
+function closeAlert() {
+    const formRoasting = document.querySelector('.form-roasting');
+    formRoasting.innerHTML = originalRoastingHTML;
+
+    document.getElementById('customAlert').style.display = 'none';
 }
